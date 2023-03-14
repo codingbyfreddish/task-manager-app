118b6769f1b7e513152a1015ee1d7f7e4a3aef88
Fredrik Gustafsson <freddish@hotmail.com>
2023-03-14 15:41:00 +0000

    fixed form issue

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
 
 
 app = Flask(__name__)
@@ -12,4 +12,3 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
-
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+});
   
