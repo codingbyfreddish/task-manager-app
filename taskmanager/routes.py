6f17129feec44be3d4c5b2109e508af37895a9cd
Fredrik Gustafsson <freddish@hotmail.com>
2023-03-22 15:06:25 +0000

    add delete task funcunality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -48,10 +48,10 @@ def add_task():
     categories = list(Category.query.order_by(Category.category_name).all())
     if request.method == "POST":
         task = Task(
-            task_name=request.form.get("task_name"), 
-            task_description=request.form.get("task_description"), 
-            is_urgent=bool(True if request.form.get("is_urgent") else False), 
-            due_date=request.form.get("due_date"), 
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
+            is_urgent=bool(True if request.form.get("is_urgent") else False),
+            due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
         )
         db.session.add(task)
@@ -72,3 +72,11 @@ def edit_task(task_id):
         task.category_id = request.form.get("category_id")
         db.session.commit()
     return render_template("edit_task.html", task=task, categories=categories)
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
